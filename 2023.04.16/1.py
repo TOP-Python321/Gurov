24839ecdcc752154dfedbe5166781bb226041639
Gurov <129942159+SlavaGurov@users.noreply.github.com>
2023-04-26 13:59:16 +0500

    2023.04.16: правки

--- 2023.04.16/1.py
+++ 2023.04.16/1.py
@@ -4,20 +4,17 @@ num_2 = input('Введите третье число: ')
 summa_positiv = 0
 
 # ИСПРАВИТЬ: две проверки избыточны, поскольку у вас нет особого требования работать с int объектами — достаточно только второй проверки
-if num.isdecimal():
-    summa_positiv += int(num)
+
 # КОММЕНТАРИЙ: метод replace() вернёт исходную строку, если не найдёт в ней переданную подстроку
-elif num.replace('.', '', 1).isdecimal():
+if num.replace('.', '', 1).isdecimal():
     summa_positiv += float(num)
 
-if num_1.isdecimal():
-    summa_positiv += int(num_1)
-elif num_1.replace('.', '', 1).isdecimal():
+
+if num_1.replace('.', '', 1).isdecimal():
     summa_positiv += float(num_1)
 
-if num_2.isdecimal():
-    summa_positiv += int(num_2)
-elif num_2.replace('.', '', 1).isdecimal():
+
+if num_2.replace('.', '', 1).isdecimal():
     summa_positiv += float(num_2)
 
 print(f'{summa_positiv:.1f}')
